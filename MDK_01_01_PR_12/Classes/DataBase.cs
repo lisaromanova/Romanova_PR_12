b9a7fbf3c66ba78e32d2fb18ed5c2a7f3e79a306
Елизавета Романова <soika-peresmeshnica2015@yandex.ru>
2022-11-29 18:08:47 +0300

    Добавьте файлы проекта.

---
+++ MDK_01_01_PR_12/Classes/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDK_01_01_PR_12.Classes
+{
+    internal class DataBase
+    {
+    }
+}
---
+++ MDK_01_01_PR_12/Classes/FrameLoad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MDK_01_01_PR_12.Classes
+{
+    internal class FrameLoad
+    {
+        public static Frame frmMain;
+    }
+}
