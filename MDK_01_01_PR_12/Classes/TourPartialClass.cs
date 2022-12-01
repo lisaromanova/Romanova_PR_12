60b4477520953b4964c8277eb49b237b3fa971d6
Елизавета Романова <soika-peresmeshnica2015@yandex.ru>
2022-12-01 16:37:33 +0300

    CommitListView2

--- MDK_01_01_PR_12/Classes/TourPartialClass.cs
+++ MDK_01_01_PR_12/Classes/TourPartialClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace MDK_01_01_PR_12
 {
@@ -15,6 +16,34 @@ namespace MDK_01_01_PR_12
                 int t = Convert.ToInt32(Math.Floor(Price / 1000));
                 int r = Convert.ToInt32(Price % 1000);
                 return t.ToString() + "," + r.ToString() + " РУБ";
+            }
+        }
+        public string ActualString
+        {
+            get
+            {
+                if (IsActual)
+                {
+                    return "Актуален";
+                }
+                else
+                {
+                    return "Не актуален";
+                }
+            }
+        }
+        public SolidColorBrush ActualColor
+        {
+            get
+            {
+                if (IsActual)
+                {
+                    return Brushes.Green;
+                }
+                else
+                {
+                    return Brushes.Red;
+                }
             }
         }
     }
--- MDK_01_01_PR_12/Pages/ToursPage.xaml.cs
+++ MDK_01_01_PR_12/Pages/ToursPage.xaml.cs
@@ -25,6 +25,46 @@ namespace MDK_01_01_PR_12.Pages
         {
             InitializeComponent();
             lstTours.ItemsSource = DataBase.connect.Tour.ToList();
+            cbType.Items.Add("Все типы");
+            List<Type> types = DataBase.connect.Type.ToList();
+            foreach(Type type in types)
+            {
+                cbType.Items.Add(type.Name);
+            }
+        }
+
+        void Filter()
+        {
+            List<Tour> tours = DataBase.connect.Tour.ToList();
+            if(cbType.SelectedIndex!=0 && cbType.SelectedIndex != -1)
+            {
+                List<int> types = DataBase.connect.TypeOfTour.Where(x=> x.Type.Name==(string)cbType.SelectedValue).Select(x=> x.TourId).ToList();
+                tours = tours.Where(x=> types.Contains(x.Id)).ToList();
+            }
+            if (!string.IsNullOrWhiteSpace(tbSearch.Text))
+            {
+                tours = tours.Where(x => x.Name.ToLower().Contains(tbSearch.Text.ToLower()) || x.Description.ToLower().Contains(tbSearch.Text.ToLower())).ToList();
+            }
+            if ((bool)chbActual.IsChecked)
+            {
+                tours = tours.Where(x => x.IsActual == true).ToList();
+            }
+            lstTours.ItemsSource = tours;
+        }
+
+        private void cbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void chbActual_Checked(object sender, RoutedEventArgs e)
+        {
+            Filter();
         }
     }
 }
